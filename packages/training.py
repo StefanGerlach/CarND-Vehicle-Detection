30c2b6bbe5265dfd9cb2985886c5f8b7e093b4f1
Stefan Gerlach <stefan.gerlach1@gmx.de>
2018-01-22 21:54:44 +0100

    Starting the final project of term 1 !
    - Implementation of the training pipeline
    - Feature Calculation

--- packages/training.py
+++ packages/training.py
@@ -1,8 +1,12 @@
 import numpy as np
 import cv2
+import pickle
+import os
 
+from tqdm import tqdm
 from sklearn import tree, ensemble, svm
 from sklearn.preprocessing import LabelEncoder
+from skimage.feature import hog
 
 from packages.class_equalizer import ClassEqualizer
 from packages.batchgenerator import BatchGenerator
@@ -28,7 +32,6 @@ label_enc.fit([k for k in training_dict])
 # Re-arrange the dictionaries for better usability in the next section.
 train_x = []
 train_y = []
-
 val_x = []
 val_y = []
 for i, c_set in enumerate([training_dict, validation_dict]):
@@ -73,14 +76,76 @@ training_batchgen = BatchGenerator(batch_size=1, n_classes=len(label_enc.classes
                                    extract_xy_fn=extract_xy_fn,
                                    shuffle=True, seed=1337)
 
-for i in range(training_batchgen.n):
-    x, y = training_batchgen.custom_next()
-    cv2.imwrite('test_'+str(i)+'.png', x[0, :, :, :])
-    if i > 100:
-        break
+# And the Batch-Generator for the validation data
+val_batchgen = BatchGenerator(batch_size=1, n_classes=len(label_enc.classes_), dataset=list(zip(val_x, val_y)),
+                              augmentation_fn=None,
+                              preprocessing_fn=preprocessing,
+                              extract_xy_fn=extract_xy_fn,
+                              shuffle=False)
 
-# Create HOG Descriptors for each set with some different parameters to
-# 'grid-search' the best parameters for the HOG Descriptor.
+# Debug writing of augmented images
+if False:
+    for i in range(training_batchgen.n):
+        x, y = training_batchgen.custom_next()
+        cv2.imwrite('test_'+str(i)+'.png', x[0, :, :, :])
+        if i > 100:
+            break
 
+# Create HOG Descriptors for each set with some different parameters to 'grid-search' the best parameters
+# for the HOG Descriptor. So lets create some ranges for num orientations, pixels_per_cell, cells_per_block and norm:
+normalize = [False]  #[True, False]
+orientations = [8]  # [8, 12, 16]
+pixels_per_cell = [(32, 32)]  #[(8, 8), (16, 16), (32, 32)]
+cells_per_block = [(1, 1)]  #[(1, 1), (2, 2)]
+
+# Lets push this into an list of dictionaries, so each permutation has its own dictionary
+permutations = [dict({'orientation': orient, 'pixels_per_cell': px_per_cell,
+                      'cells_per_block': cells_pb, 'normalize': norm, 'train_features': [], 'val_features': []})
+                for orient in orientations
+                for px_per_cell in pixels_per_cell
+                for cells_pb in cells_per_block
+                for norm in normalize]
+
+# Now we have to compute the HOG Features for train_x and val_x for every parameter set!
+# Check if we already have computed them:
+feature_file_name = 'feature_permutation_checkpoint.picklefile'
+if os.path.isfile(feature_file_name):
+    # Yes, load them !
+    permutations = pickle.load(open(feature_file_name, 'rb'))
+    print('Loaded ' + str(len(permutations)) + ' parameter sets with training and val feature vectors.')
+else:
+    # Take all the training images and create the features:
+    for i in tqdm(range(training_batchgen.n)):
+        x, _ = training_batchgen.custom_next()
+        # Go over parameter sets
+        for parameter_set in permutations:
+            feature_descriptor = hog(image=cv2.cvtColor(np.uint8(x[0]), cv2.COLOR_RGB2GRAY),
+                                     orientations=parameter_set['orientation'],
+                                     pixels_per_cell=parameter_set['pixels_per_cell'],
+                                     cells_per_block=parameter_set['cells_per_block'],
+                                     transform_sqrt=parameter_set['normalize'],
+                                     visualise=False)
+
+            parameter_set['train_features'].append(feature_descriptor)
+
+    for i in tqdm(range(val_batchgen.n)):
+        x, _ = val_batchgen.custom_next()
+        # Go over parameter sets
+        for parameter_set in permutations:
+            feature_descriptor = hog(image=cv2.cvtColor(np.uint8(x[0]), cv2.COLOR_RGB2GRAY),
+                                     orientations=parameter_set['orientation'],
+                                     pixels_per_cell=parameter_set['pixels_per_cell'],
+                                     cells_per_block=parameter_set['cells_per_block'],
+                                     transform_sqrt=parameter_set['normalize'],
+                                     visualise=False)
+
+            parameter_set['val_features'].append(feature_descriptor)
+
+    print('Dumping feature vectors..')
+    pickle.dump(permutations, open(feature_file_name, 'wb'))
+
+
+# Now we want to know how these combinations of parameters of the HOG Descriptor performs
+# for every tested classifier method.
 
 i = 100
